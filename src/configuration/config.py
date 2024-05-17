73c82cdb70f5b2510e0f7b2f84b56c03f6d76413
Venura Pussella <137409412+Venura-94@users.noreply.github.com>
2024-05-17 17:11:38 +0530

    removed config.py data

--- src/configuration/config.py
+++ src/configuration/config.py
@@ -1,4 +1,4 @@
-GSHEET_CREDS_PATH = './src/configuration/gsheet_api_key.json'
-SPREADSHEET_NAME = 'gpt-email-bot-sample'
-SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1OolRBDrbs0N_YgHMPOzDNy0IKsCvoB_v4GJ3K4uRaFI/edit?usp=sharing'
-WORKSHEET_NAME = 'Invoice-client-list'
+GSHEET_CREDS_PATH = ''
+SPREADSHEET_NAME = ''
+SPREADSHEET_URL = ''
+WORKSHEET_NAME = ''
